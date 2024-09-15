cfc74dd917ee829a64acab717870bd2b6c493a96
kent.huang <kenthuang727@gmail.com>
2024-09-14 17:11:54 -0700

    wip Home Component

--- portfolio_website/src/components/home/Home.jsx
+++ portfolio_website/src/components/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./home.css"
+import Me from "../../assets/avatar-1.svg"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Kent H</h1>
+        <span className="home__education">I'm a developer</span>
+      </div>
+    </section>
   )
 }
 
