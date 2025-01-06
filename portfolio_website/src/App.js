3f1e3ebb1b29b04aa985e89792ee9d62172b2317
kent.huang <kenthuang727@gmail.com>
2025-01-05 23:54:32 -0800

    implement responsiveness for smaller resolutions

--- portfolio_website/src/App.js
+++ portfolio_website/src/App.js
@@ -15,22 +15,6 @@ import Contact from './components/contact/Contact';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <>
     <Sidebar></Sidebar>
     <main className='main'>
